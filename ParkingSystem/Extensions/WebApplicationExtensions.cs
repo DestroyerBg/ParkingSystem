8fa5edaf6f0dd88c077b6a78feef3295e4441d05
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2025-02-22 19:49:26 +0200

    Update WebApplicationExtensions.cs

--- ParkingSystem/Extensions/WebApplicationExtensions.cs
+++ ParkingSystem/Extensions/WebApplicationExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace ParkingSystem.Extensions
+public static class WebApplicationExtensions
 {
-    public class WebApplicationExtensions
+    public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
     {
+        using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+        CarDbContext dbContext = serviceScope
+            .ServiceProvider
+            .GetRequiredService<CarDbContext>();
+
+        dbContext.Database.Migrate();
+
+        return app;
     }
 }
