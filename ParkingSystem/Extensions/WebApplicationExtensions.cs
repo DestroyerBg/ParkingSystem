5e861a002041418250a89fce6129ca6a73bc73df
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2025-02-22 19:44:56 +0200

    added auto migrate when application starts

---
+++ ParkingSystem/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingSystem.Data;
+
+namespace ParkingSystem.Extensions
+{
+    public static  class WebApplicationExtensions
+    {
+        public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+            CarDbContext dbContext = serviceScope
+                .ServiceProvider
+                .GetRequiredService<CarDbContext>();
+
+            dbContext.Database.Migrate();
+
+            return app;
+        }
+    }
+}
--- ParkingSystem/Program.cs
+++ ParkingSystem/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ParkingSystem.Data;
+using ParkingSystem.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.ApplyMigrations();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
